eb2fdab95e68e5dff58863e2bbe1db1cf9b1adcd
Ondrej Zavodsky <zavodsky.ondrej1@gmail.com>
2026-06-18 13:17:29 +0200

    fix the neovim formaters

--- .dotfiles/.config/nvim/lua/plugins/formaters.lua
+++ .dotfiles/.config/nvim/lua/plugins/formaters.lua
@@ -1,35 +1,51 @@
 return {
 	"stevearc/conform.nvim",
 	event = { "BufWritePre" },
-	opts = {
-		format_on_save = {
-			timeout_ms = 500,
-			lsp_format = "fallback",
-		},
-		formatters = {
-			prettier = {
-				prepend_args = { "--use-tabs", "--single-quote" },
+	opts = function()
+		local opts = {
+			format_on_save = {
+				timeout_ms = 500,
+				lsp_format = "fallback",
 			},
-			black = {
-				prepend_args = { "--line-length", "88" },
+			formatters = {
+				prettier = {
+					prepend_args = { "--use-tabs", "--single-quote" },
+				},
+				black = {
+					prepend_args = { "--line-length", "88" },
+				},
+				stylua = {
+					prepend_args = { "--indent-type", "Tabs", "--indent-width", "1" },
+				},
 			},
-			stylua = {
-				prepend_args = { "--indent-type", "Tabs", "--indent-width", "1" },
+			formatters_by_ft = {
+				lua = { "stylua" },
+				python = { "black", "isort" },
+				rust = { "rustfmt" },
+				sql = { "sqlfluff" },
+				json = { "prettier" },
+				yaml = { "prettier" },
+				markdown = { "prettier" },
+				html = { "prettier" },
+				css = { "prettier" },
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				java = { "google-java-format" },
 			},
-		},
-		formatters_by_ft = {
-			lua = { "stylua" },
-			python = { "black", "isort" },
-			rust = { "rustfmt" },
-			sql = { "sqlfluff" },
-			json = { "prettier" },
-			yaml = { "prettier" },
-			markdown = { "prettier" },
-			html = { "prettier" },
-			css = { "prettier" },
-			javascript = { "prettier" },
-			typescript = { "prettier" },
-			java = { "google-java-format" },
-		},
-	},
+		}
+
+		for _, formatters in pairs(opts.formatters_by_ft) do
+			for _, formatter_name in ipairs(formatters) do
+				opts.formatters[formatter_name] = opts.formatters[formatter_name] or {}
+
+				opts.formatters[formatter_name].command = formatter_name
+
+				opts.formatters[formatter_name].condition = function()
+					return vim.fn.executable(formatter_name) == 1
+				end
+			end
+		end
+
+		return opts
+	end,
 }
