a86699743368dccfb47a30ae4fcf019ad99be571
Ondrej Zavodsky <zavodsky.ondrej1@gmail.com>
2026-06-18 10:04:40 +0200

    changed the nvim settings to fit the nix so i can use lsp

--- .dotfiles/.config/nvim/lua/plugins/java.lua
+++ .dotfiles/.config/nvim/lua/plugins/java.lua
@@ -3,17 +3,26 @@ return {
 		"mfussenegger/nvim-jdtls",
 		ft = "java",
 		config = function()
-			local bundles = {}
+			local home = os.getenv("HOME")
+			local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+			local workspace_dir = home .. "/.cache/jdtls/workspace/" .. project_name
 
-			-- add java-debug-adapter
-			local mason_path = vim.fn.stdpath("data") .. "/mason"
-			vim.list_extend(
-				bundles,
-				vim.split(vim.fn.glob(mason_path .. "/share/java-debug-adapter/com.microsoft.java.debug.plugin-*.jar"), "\n")
-			)
+			local bundles = {}
+			local nix_debug_path = "/run/current-system/sw/share/java-debug-adapter"
+
+			if vim.fn.empty(vim.fn.glob(nix_debug_path)) == 0 then
+				vim.list_extend(bundles, vim.split(vim.fn.glob(nix_debug_path .. "/com.microsoft.java.debug.plugin-*.jar"), "\n"))
+			else
+				local mason_path = vim.fn.stdpath("data") .. "/mason"
+				vim.list_extend(
+					bundles,
+					vim.split(vim.fn.glob(mason_path .. "/share/java-debug-adapter/com.microsoft.java.debug.plugin-*.jar"), "\n")
+				)
+			end
 
 			local config = {
-				cmd = { mason_path .. "/bin/jdtls" },
+				cmd = { "jdtls", "-data", workspace_dir },
+
 				root_dir = vim.fs.dirname(
 					vim.fs.find({ "gradlew", "mvnw", ".git", "pom.xml", "build.gradle" }, { upward = true })[1]
 				),
--- shells/java.nix
+++ shells/java.nix
@@ -4,6 +4,8 @@ pkgs.mkShell {
   packages = with pkgs; [
     jdk21
     gradle
+	 jdt-language-server
+	 google-java-format
   ];
   shellHook = ''echo "java development environment"
 	exec zsh
