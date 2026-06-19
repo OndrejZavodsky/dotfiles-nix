d43a832e413401dd2de94f2aa0bcea4cf0fd64ab
Ondrej Zavodsky <zavodsky.ondrej1@gmail.com>
2026-06-19 07:01:19 +0200

    atempted to fix the issue with the powermeny launch

--- desctop.nix
+++ desctop.nix
@@ -1,31 +1,31 @@
-{ config, pkgs, ... }: 
-
 {
-
+  config,
+  pkgs,
+  ...
+}: {
   programs.hyprland = {
-	enable = true;
-	xwayland.enable = true;
+    enable = true;
+    xwayland.enable = true;
   };
-	programs.hyprlock.enable = true;
-	security.pam.services.hyprlock = {};
-	services.greetd = {
-  enable = true;
-  settings = {
-    initial_session = {
-      command = "start-hyprland";
-      user = "ondrejz"; 
+  programs.hyprlock.enable = true;
+  security.pam.services.hyprlock = {};
+  services.greetd = {
+    enable = true;
+    settings = {
+      initial_session = {
+        command = "start-hyprland";
+        user = "ondrejz";
+      };
+      default_session = {
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --cmd Hyprland";
+        user = "greeter";
+      };
     };
-    default_session = {
-      command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --cmd Hyprland";
-      user = "greeter";
-    		};
-  		};
-	};
-	environment.systemPackages = with pkgs; [
-		kitty
-		waybar
-		hypridle
-		hyprpaper
-	];
-
+  };
+  environment.systemPackages = with pkgs; [
+    kitty
+    waybar
+    hypridle
+    hyprpaper
+  ];
 }
