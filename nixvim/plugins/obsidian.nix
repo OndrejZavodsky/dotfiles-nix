c889372e67a8fef9bb7c260341968fcf21423cf7
Ondrej Zavodsky <zavodsky.ondrej1@gmail.com>
2026-06-22 09:50:06 +0200

    renamed the notes forder

--- nixvim/plugins/obsidian.nix
+++ nixvim/plugins/obsidian.nix
@@ -4,17 +4,6 @@
   ...
 }: {
   programs.nixvim.opts.conceallevel = 1;
-  programs.nixvim.extraConfigLua = ''
-    vim.api.nvim_create_autocmd("LspAttach", {
-      callback = function(ev)
-        local client = vim.lsp.get_client_by_id(ev.data.client_id)
-        if client and client.name == "obsidian-ls" then
-          -- Force completion to trigger for these characters
-          client.server_capabilities.completionProvider.triggerCharacters = { "[", "/", "#" }
-        end
-      end,
-    })
-  '';
   programs.nixvim.plugins.obsidian = {
     enable = true;
 
@@ -23,11 +12,11 @@
       workspaces = [
         {
           name = "personal";
-          path = "~/Notes/personal";
+          path = "~/notes/personal";
         }
         {
           name = "work_and_school";
-          path = "~/Notes/work_and_school";
+          path = "~/notes/work_and_school";
         }
       ];
 
