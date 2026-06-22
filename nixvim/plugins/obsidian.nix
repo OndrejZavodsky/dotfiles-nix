effee95c71ef30293d98bc2054db1aeb9186ad1a
Ondrej Zavodsky <zavodsky.ondrej1@gmail.com>
2026-06-22 08:18:01 +0200

    try to fix the obsidian linking suggestions

--- nixvim/plugins/obsidian.nix
+++ nixvim/plugins/obsidian.nix
@@ -4,6 +4,17 @@
   ...
 }: {
   programs.nixvim.opts.conceallevel = 1;
+  programs.nixvim.extraConfigLua = ''
+    vim.api.nvim_create_autocmd("LspAttach", {
+      callback = function(ev)
+        local client = vim.lsp.get_client_by_id(ev.data.client_id)
+        if client and client.name == "obsidian-ls" then
+          -- Force completion to trigger for these characters
+          client.server_capabilities.completionProvider.triggerCharacters = { "[", "/", "#" }
+        end
+      end,
+    })
+  '';
   programs.nixvim.plugins.obsidian = {
     enable = true;
 
